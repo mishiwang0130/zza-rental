5d0e466865ddb022b610545276526cfa399fad80
wxy <2956804893@qq.com>
2026-08-22 14:53:06 +0800

    [A] init

---
+++ src/main/java/com/wxy/zzarental/ZzaRentalApplication.java
@@ -0,0 +1,13 @@
+package com.wxy.zzarental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzaRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzaRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wxy/zzarental/ZzaRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxy.zzarental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzaRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

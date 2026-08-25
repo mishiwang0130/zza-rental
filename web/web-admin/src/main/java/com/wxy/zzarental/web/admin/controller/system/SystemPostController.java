af22f621e8f25d21a6fe82021626dec12a98c89e
wxy <2956804893@qq.com>
2026-08-25 14:56:34 +0800

    [A] 系统管理-岗位管理

--- web/web-admin/src/main/java/com/wxy/zzarental/web/admin/controller/system/SystemPostController.java
+++ web/web-admin/src/main/java/com/wxy/zzarental/web/admin/controller/system/SystemPostController.java
@@ -1,11 +1,16 @@
 package com.wxy.zzarental.web.admin.controller.system;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wxy.zzarental.common.result.Result;
+import com.wxy.zzarental.model.entity.BaseEntity;
 import com.wxy.zzarental.model.entity.SystemPost;
 import com.wxy.zzarental.model.enums.BaseStatus;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.wxy.zzarental.web.admin.service.SystemPostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,21 +21,28 @@ import java.util.List;
 @RequestMapping("/admin/system/post")
 public class SystemPostController {
 
+    @Resource
+    private SystemPostService systemPostService;
+
     @Operation(summary = "分页获取岗位信息")
     @GetMapping("page")
     private Result<IPage<SystemPost>> page(@RequestParam long current, @RequestParam long size) {
-        return Result.ok();
+        IPage<SystemPost> systemPostPage = new Page<>(current, size);
+        IPage<SystemPost> page = systemPostService.page(systemPostPage);
+        return Result.ok(page);
     }
 
     @Operation(summary = "保存或更新岗位信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody SystemPost systemPost) {
+        systemPostService.saveOrUpdate(systemPost);
         return Result.ok();
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据id删除岗位")
     public Result removeById(@RequestParam Long id) {
+        systemPostService.removeById(id);
 
         return Result.ok();
     }
@@ -38,18 +50,31 @@ public class SystemPostController {
     @GetMapping("getById")
     @Operation(summary = "根据id获取岗位信息")
     public Result<SystemPost> getById(@RequestParam Long id) {
-        return Result.ok();
+        return Result.ok(systemPostService.getById(id));
     }
 
     @Operation(summary = "获取全部岗位列表")
     @GetMapping("list")
     public Result<List<SystemPost>> list() {
+        systemPostService.list();
         return Result.ok();
     }
 
     @Operation(summary = "根据岗位id修改状态")
     @PostMapping("updateStatusByPostId")
     public Result updateStatusByPostId(@RequestParam Long id, @RequestParam BaseStatus status) {
+        // 这里是根据id来修改
+        LambdaUpdateWrapper<SystemPost> systemPostLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        systemPostLambdaUpdateWrapper.eq(BaseEntity::getId,id);
+        systemPostLambdaUpdateWrapper.set(SystemPost::getStatus,status);
+        systemPostService.update(systemPostLambdaUpdateWrapper);
+        // selectById和removeById是传id去做操作，updateById是传一个实体，他会把id当条件，把其他“非空”字段全部set一次
+        // 通过updateById去做修改的时候，不会把null值的字段拼接到这条update语句的set上
+//        SystemPost systemPost = new SystemPost();
+//        systemPost.setId(id);
+//        systemPost.setName("aaa");
+//        systemPost.setStatus(status);
+//        systemPostService.updateById(systemPost);
         return Result.ok();
     }
 }

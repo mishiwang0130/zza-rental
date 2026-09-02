110e3421fe0793c484669fb575a6bb5a30689091
wxy <2956804893@qq.com>
2026-09-02 18:03:41 +0800

    [A] 移动端-短信验证码

--- common/src/main/java/com/wxy/zzarental/common/result/ResultCodeEnum.java
+++ common/src/main/java/com/wxy/zzarental/common/result/ResultCodeEnum.java
@@ -37,6 +37,7 @@ public enum ResultCodeEnum {
     APP_LOGIN_CODE_EXPIRED(505, "验证码已过期"),
     APP_LOGIN_CODE_ERROR(506, "验证码错误"),
     APP_ACCOUNT_DISABLED_ERROR(507, "该用户已被禁用"),
+    APP_SEND_SMS_ERROR(508, "验证码发送失败"),
 
 
     TOKEN_EXPIRED(601, "token过期"),
--- web/web-app/src/main/java/com/wxy/zzarental/web/app/service/impl/SmsServiceImpl.java
+++ web/web-app/src/main/java/com/wxy/zzarental/web/app/service/impl/SmsServiceImpl.java
@@ -1,9 +1,58 @@
 package com.wxy.zzarental.web.app.service.impl;
 
+import com.aliyun.tea.TeaException;
+import com.wxy.zzarental.common.exception.ZZAException;
+import com.wxy.zzarental.common.result.ResultCodeEnum;
 import com.wxy.zzarental.web.app.service.SmsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmsServiceImpl implements SmsService {
 
+    @Override
+    public void sendCode(String phone, String code) {
+        // 普通方法调用普通方法不需要通过类名.方法
+        com.aliyun.dypnsapi20170525.Client client = null;
+        try {
+            client = createClient();
+        } catch (Exception e) {
+            throw new ZZAException(ResultCodeEnum.APP_SEND_SMS_ERROR);
+        }
+        String templateParam = "{\"code\":\""+ code + "\",\"min\":\"5\"}";
+        com.aliyun.dypnsapi20170525.models.SendSmsVerifyCodeRequest sendSmsVerifyCodeRequest = new com.aliyun.dypnsapi20170525.models.SendSmsVerifyCodeRequest()
+                .setSignName("恒创联众")
+                .setTemplateCode("100001")
+                .setPhoneNumber(phone)
+                .setTemplateParam(templateParam);
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        try {
+            com.aliyun.dypnsapi20170525.models.SendSmsVerifyCodeResponse resp = client.sendSmsVerifyCodeWithOptions(sendSmsVerifyCodeRequest, runtime);
+            System.out.println(new com.google.gson.Gson().toJson(resp));
+        } catch (TeaException error) {
+            // 此处仅做打印展示，请谨慎对待异常处理，在工程项目中切勿直接忽略异常。
+            // 错误 message
+            System.out.println(error.getMessage());
+            // 诊断地址
+            System.out.println(error.getData().get("Recommend"));
+        } catch (Exception _error) {
+            TeaException error = new TeaException(_error.getMessage(), _error);
+            // 此处仅做打印展示，请谨慎对待异常处理，在工程项目中切勿直接忽略异常。
+            // 错误 message
+            System.out.println(error.getMessage());
+            // 诊断地址
+            System.out.println(error.getData().get("Recommend"));
+        }
+
+    }
+
+
+    private com.aliyun.dypnsapi20170525.Client createClient() throws Exception {
+        // 工程代码建议使用更安全的无AK方式，凭据配置方式请参见：https://help.aliyun.com/document_detail/378657.html。
+        com.aliyun.credentials.Client credential = new com.aliyun.credentials.Client();
+        com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
+                .setCredential(credential);
+        // Endpoint 请参考 https://api.aliyun.com/product/Dypnsapi
+        config.endpoint = "dypnsapi.aliyuncs.com";
+        return new com.aliyun.dypnsapi20170525.Client(config);
+    }
 }

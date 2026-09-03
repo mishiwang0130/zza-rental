67bb9aa24becb80ce1faa64bfd979b4ea3f91de7
wxy <2956804893@qq.com>
2026-09-03 11:17:02 +0800

    [A] 移动端-支付方式接口

--- web/web-app/src/main/java/com/wxy/zzarental/web/app/controller/payment/PaymentTypeController.java
+++ web/web-app/src/main/java/com/wxy/zzarental/web/app/controller/payment/PaymentTypeController.java
@@ -1,10 +1,16 @@
 package com.wxy.zzarental.web.app.controller.payment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.wxy.zzarental.common.result.Result;
+import com.wxy.zzarental.model.entity.BaseEntity;
 import com.wxy.zzarental.model.entity.PaymentType;
+import com.wxy.zzarental.model.entity.RoomPaymentType;
+import com.wxy.zzarental.web.app.service.PaymentTypeService;
+import com.wxy.zzarental.web.app.service.RoomPaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +22,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/app/payment")
 public class PaymentTypeController {
+    @Resource
+    private PaymentTypeService paymentTypeService;
+    @Resource
+    private RoomPaymentTypeService roomPaymentTypeService;
 
     @Operation(summary = "根据房间id获取可选支付方式列表")
     @GetMapping("listByRoomId")
     public Result<List<PaymentType>> list(@RequestParam Long id) {
-        return Result.ok();
+        return Result.ok(paymentTypeService.getPaymentTypeByRoomId(id));
     }
 
     @Operation(summary = "获取全部支付方式列表")
     @GetMapping("list")
     public Result<List<PaymentType>> list() {
-        return Result.ok();
+        return Result.ok(paymentTypeService.list());
     }
 }
--- web/web-app/src/main/java/com/wxy/zzarental/web/app/service/impl/PaymentTypeServiceImpl.java
+++ web/web-app/src/main/java/com/wxy/zzarental/web/app/service/impl/PaymentTypeServiceImpl.java
@@ -1,10 +1,21 @@
 package com.wxy.zzarental.web.app.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.ListUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.wxy.zzarental.model.entity.BaseEntity;
 import com.wxy.zzarental.model.entity.PaymentType;
+import com.wxy.zzarental.model.entity.RoomPaymentType;
 import com.wxy.zzarental.web.app.mapper.PaymentTypeMapper;
 import com.wxy.zzarental.web.app.service.PaymentTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wxy.zzarental.web.app.service.RoomPaymentTypeService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
 * @author liubo
@@ -14,7 +25,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentTypeServiceImpl extends ServiceImpl<PaymentTypeMapper, PaymentType>
     implements PaymentTypeService{
+    @Resource
+    private RoomPaymentTypeService roomPaymentTypeService;
+    @Resource
+    private PaymentTypeMapper paymentTypeMapper;
 
+    @Override
+    public List<PaymentType> getPaymentTypeByRoomId(Long id) {
+        //先拿到关联表
+        LambdaQueryWrapper<RoomPaymentType> roomPaymentTypeLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        roomPaymentTypeLambdaQueryWrapper.eq(RoomPaymentType::getId,id);
+        List<RoomPaymentType> roomList = roomPaymentTypeService.list(roomPaymentTypeLambdaQueryWrapper);
+        //拿到关联表中的paymentid
+        List<Long> paymentIdList = roomList.stream().map(RoomPaymentType::getPaymentTypeId).distinct().collect(Collectors.toList());
+        //判断该房间有没有支付方式
+        if(CollUtil.isEmpty(paymentIdList)){
+            return Collections.emptyList();
+        }
+        //通过查id在不在paymentidlist里面判断
+
+        LambdaQueryWrapper<PaymentType> paymentTypeLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        paymentTypeLambdaQueryWrapper.in(BaseEntity::getId,paymentIdList);
+
+//        return paymentTypeMapper.selectList(paymentTypeLambdaQueryWrapper);
+        // paymentTypeService.list()是通过对象.方法来调用PaymentTypeService类里面的list方法
+        // 我们本来就是PaymentTypeService类，所以相当于调用自己的list方法
+        return list(paymentTypeLambdaQueryWrapper);
+    }
 }
 
 
